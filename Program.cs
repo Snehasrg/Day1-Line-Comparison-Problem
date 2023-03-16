200e1d96c67d6d9f5aa9fca25cacc7739e131da4
Snehasrg <sghongade642@gmail.com>
2023-03-16 06:59:13 +0530

    Mthod Calling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Problem");
+            
+            Length.calculatelength();
         }
     }
 }
